bf5dea336be82806ae42e7a9d9cdd7f5ba218ca4
Toshimi <toshimikusunoki@gmail.com>
2020-02-15 14:51:36 +0900

    Vue.Draggable追加

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,6 +6,10 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
+    <h3>Vue.js サンプル</h3>
+    <ul>
+      <li><router-link to="/draganddrop">ドラッグ&ドロップ</router-link></li>
+    </ul>
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from "./components/HelloWorld"
+import DragAndDrop from "./components/DragAndDrop";
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
-    { path: '/', component: HelloWorld }
+    { path: '/', component: HelloWorld },
+    { path: '/draganddrop', component: DragAndDrop}
   ]
 })
