a4b286f8d999f89ec406b5aeb16c98a5885485f1
Toshimi <toshimikusunoki@gmail.com>
2020-02-15 15:02:54 +0900

    ElementUI追加

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from "./router";
+import router from "./router"
+
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/ja'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI, {locale})
 
 new Vue({
   router,
