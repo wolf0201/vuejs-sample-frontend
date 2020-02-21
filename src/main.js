e0c1a9ba1b1500bedd0beb34d659600996ee41a9
Toshimi <toshimikusunoki@gmail.com>
2020-02-22 00:01:17 +0900

    ElementUI追加

--- src/main.js
+++ src/main.js
@@ -6,10 +6,16 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue'
 
+import ElementUI from 'element-ui'
+import locale from "element-ui/src/mixins/locale";
+import 'element-ui/lib/theme-chalk/index.css'
+
 Vue.config.productionTip = false
 
 // Bootstrap
 Vue.use(BootstrapVue)
+
+Vue.use(ElementUI, {locale})
 
 new Vue({
   router,
